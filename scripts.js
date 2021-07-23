3eb7f3b032efe2030a58614c3ac098a8d158eb79
Gian <giandcarvalho@gmail.com>
2021-07-23 12:25:20 -0300

    Added overlay and send order button

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
-function calcTotalOrder() {
-  let dishValue = document.querySelector('input[name="dishes"]:checked').value;
-  let drinkValue = document.querySelector('input[name="drink"]:checked').value;
-  let dessertValue = document.querySelector(
-    'input[name="dessert"]:checked'
-  ).value;
+// function calcTotalOrder() {
+//   let dishValue = document.querySelector('input[name="dishes"]:checked').value;
+//   let drinkValue = document.querySelector('input[name="drink"]:checked').value;
+//   let dessertValue = document.querySelector(
+//     'input[name="dessert"]:checked'
+//   ).value;
 
-  alert(Number(dishValue) + Number(drinkValue) + Number(dessertValue));
+//   alert(Number(dishValue) + Number(drinkValue) + Number(dessertValue));
+// }
+
+// function getItemName() {
+//   let productName = document.querySelector(
+//     'input[name="dishes"]:checked + .option > .product-name'
+//   ).innerText;
+
+//   alert(productName);
+// }
+
+// readyToOrder();
+
+// function readyToOrder() {
+//   const button = document.querySelector(".checkout);
+//   while (true) {
+//     if (
+//       document.querySelector('input[name="dishes"]:checked') !== null &&
+//       document.querySelector('input[name="drink"]:checked') !== null &&
+//       document.querySelector('input[name="dessert"]:checked') !== null
+//     ) {
+//       button.classList.add("active");
+//     } else {
+//       continue;
+//     }
+//   }
 }
