96c340333936fa6c365bb99e4ba8b2ab50fe6b55
Gian <giandcarvalho@gmail.com>
2021-07-24 00:01:12 -0300

    Replaced input radios with regular buttons and added new scripts

--- scripts.js
+++ scripts.js
@@ -1,34 +1,71 @@
-// function calcTotalOrder() {
-//   let dishValue = document.querySelector('input[name="dishes"]:checked').value;
-//   let drinkValue = document.querySelector('input[name="drink"]:checked').value;
-//   let dessertValue = document.querySelector(
-//     'input[name="dessert"]:checked'
-//   ).value;
+function selectButton(button) {
+  let alreadySelected = document.querySelector(
+    "." + button.classList[0] + ".selected"
+  );
+  if (alreadySelected !== null) {
+    document;
+    alreadySelected.classList.remove("selected");
+  }
+  button.classList.add("selected");
+  enableCheckoutButton();
+  addToOrder(button);
+}
 
-//   alert(Number(dishValue) + Number(drinkValue) + Number(dessertValue));
-// }
+function addToOrder(item) {
+  let className = "." + item.classList[0];
+  let itemNameOrder = document.querySelector(
+    ".order-items " + className + " > .name"
+  );
+  let itemPriceOrder = document.querySelector(
+    ".order-items " + className + " > .price"
+  );
 
-// function getItemName() {
-//   let productName = document.querySelector(
-//     'input[name="dishes"]:checked + .option > .product-name'
-//   ).innerText;
+  itemNameOrder.innerHTML = document.querySelector(
+    className + ".selected" + " .product-name"
+  ).innerHTML;
+  itemPriceOrder.innerHTML = document.querySelector(
+    className + ".selected" + " .price"
+  ).innerHTML;
+}
 
-//   alert(productName);
-// }
+const button = document.querySelector(".checkout");
+button.disable = true;
 
-// readyToOrder();
+function calcTotal() {
+  let total = document.querySelector(".total-order");
+  let firstItemPrice = document
+    .querySelector(".order-items .first > .price")
+    .innerHTML.replace(",", ".");
+  let secondItemPrice = document
+    .querySelector(".order-items .second > .price")
+    .innerHTML.replace(",", ".");
+  let thirdItemPrice = document
+    .querySelector(".order-items .third > .price")
+    .innerHTML.replace(",", ".");
 
-// function readyToOrder() {
-//   const button = document.querySelector(".checkout);
-//   while (true) {
-//     if (
-//       document.querySelector('input[name="dishes"]:checked') !== null &&
-//       document.querySelector('input[name="drink"]:checked') !== null &&
-//       document.querySelector('input[name="dessert"]:checked') !== null
-//     ) {
-//       button.classList.add("active");
-//     } else {
-//       continue;
-//     }
-//   }
+  total.innerHTML =
+    Number(firstItemPrice) + Number(secondItemPrice) + Number(thirdItemPrice);
+}
+
+function enableCheckoutButton() {
+  if (
+    document.querySelector(".first.selected") !== null &&
+    document.querySelector(".second.selected") !== null &&
+    document.querySelector(".third.selected") !== null
+  ) {
+    button.disable = false;
+    button.classList.add("active");
+    button.innerHTML = "Fechar Pedido";
+    calcTotal();
+  }
+}
+
+function openConfirmOrder() {
+  document.querySelector(".overlay").classList.add("active");
+  document.querySelector(".confirm-order").classList.add("active");
+}
+
+function cancel() {
+  document.querySelector(".overlay").classList.remove("active");
+  document.querySelector(".confirm-order").classList.remove("active");
 }
